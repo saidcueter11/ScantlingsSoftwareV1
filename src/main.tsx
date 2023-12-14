5ac7761450432cca0eaab9c278e00f89186a7263
Said Cueter <saidcueter11@gmail.com>
2023-12-14 06:54:20 -0500

    Created login

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, Outlet, Navigate } from 'react-router-dom'
 import './index.css'
 import { ScantlingsContextProvider } from './Context/ScantlingsContext.tsx'
 import Login from './Routes/Login.tsx'
 import { GeneralPage } from './Routes/General.tsx'
 import { ZonePage } from './Routes/Zone.tsx'
 import { Results } from './Routes/Results.tsx'
-import PrivateRoute from './components/PrivateRoute' // Import the PrivateRoute component
 
 const isAuthenticated = true // Replace with your authentication logic
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
+    element: isAuthenticated
+      ? (
       <Outlet />
-    ),
+        )
+      : (
+      <Navigate to="/login" />
+        ),
     children: [
       {
         path: '/',
-        element: (
-          <PrivateRoute isAuthenticated={isAuthenticated} path="/" element={<GeneralPage />} />
-        )
+        element: <GeneralPage />
       },
       {
         path: 'zone/:zoneName',
-        element: (
-          <PrivateRoute isAuthenticated={isAuthenticated} path="zone/:zoneName" element={<ZonePage />} />
-        )
+        element: <ZonePage />
       },
       {
         path: 'zone/:zoneName/results',
-        element: (
-          <PrivateRoute isAuthenticated={isAuthenticated} path="zone/:zoneName/results" element={<Results />} />
-        )
+        element: <Results />
       }
     ]
   },
